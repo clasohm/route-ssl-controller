f11b4dad4fa82f49a7abf0e276088c6374793c04
Carsten Clasohm <clasohm@redhat.com>
2019-01-22 16:53:24 -0600

    list OpenShift routes

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     config.load_incluster_config()
     api_client = client.CoreV1Api()
 
-    for event in watch.Watch().stream(api_client.list_pod_for_all_namespaces):
+    for event in watch.Watch().stream(api_client.list_cluster_custom_object('route.openshift.io', 'v1', 'routes')):
         print("Event: %s %s %s" % (event['type'],event['object'].kind, event['object'].metadata.name))
