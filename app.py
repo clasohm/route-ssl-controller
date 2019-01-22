b735a3107b77129683598ce086d90450f5a39aac
Carsten Clasohm <clasohm@redhat.com>
2019-01-22 16:42:59 -0600

    shorter code

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
-import json
-import yaml
 from kubernetes import client, config, watch
-import os
 
 if __name__ == "__main__":
-    if 'KUBERNETES_PORT' in os.environ:
-        config.load_incluster_config()
-    else:
-        config.load_kube_config()
-
+    config.load_incluster_config()
     api_client = client.CoreV1Api()
 
     for event in watch.Watch().stream(api_client.list_pod_for_all_namespaces):
