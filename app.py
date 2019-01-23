df6bc018438ca1523b9c96e28b808e5a9d7e88cf
Carsten Clasohm <clasohm@redhat.com>
2019-01-23 08:23:04 -0600

    revert to pods

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from kubernetes import client, config, watch
 if __name__ == "__main__":
     config.load_incluster_config()
     api_client = client.CoreV1Api()
-    crds = client.CustomObjectsApi(api_client)
 
-    for event in watch.Watch().stream(crds.list_cluster_custom_object('route.openshift.io', 'v1', 'routes')):
+    for event in watch.Watch().stream(api_client.list_pod_for_all_namespaces):
         print("Event: %s %s %s" % (event['type'],event['object'].kind, event['object'].metadata.name))
